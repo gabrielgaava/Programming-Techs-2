e58d68663708acc0ae30bb335ba52441d0751d40
gabrielgaava <gabrielgava@estudioddr.dev>
2020-11-30 11:28:18 -0300

    Project_3_V1 Done

--- projects/Project3/main.c
+++ projects/Project3/main.c
@@ -2,9 +2,34 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-// Global Value
-int boardSize;
+// Global Values
+int boardSize = 0;
+int solutionsCount = 0;
 int* board;
+FILE * tempFile;
+
+void createOutputFile(){
+
+    // READ BUFFER
+    char line[86];
+
+    FILE * outputFile = fopen("rainha.out", "w+");
+    tempFile = fopen("temp.txt", "r");
+
+    if(outputFile == NULL) 
+        printf(">> Erro ao criar arquivo de saida!\n");
+
+    fprintf(outputFile, "%d\n\n", solutionsCount);
+
+    while(fgets(line, sizeof(line), tempFile) != NULL){
+        printf("%s", line);
+        fprintf(outputFile, "%s", line);
+    }
+
+    fclose(tempFile);
+    fclose(outputFile);
+    remove ("temp.txt");
+}
 
 /* Init the all the board's positions with 0 */
 void initBoard(){
@@ -20,94 +45,117 @@ void initBoard(){
 }
 
 /* Print the actual state of the board in terminal */
-void printBoard(){
+void printSolution(){
+    solutionsCount++;
+    //printf("\nSolution %d: \n", solutionsCount);
     int i, j;
     for(i = 0; i < boardSize; i++){
         for(j = 0; j < boardSize; j++){
             // board[i][j]
-            printf("%d  ", board[i * boardSize + j]);
+            //printf("%d  ", board[i * boardSize + j]);
+            fprintf(tempFile, "%d ", board[i * boardSize + j]);
         }
-        printf("\n");
+        //printf("\n");
+        fprintf(tempFile, "\n");
     }
+    fprintf(tempFile, "\n\n");
 }
 
 // Check if the cell (row,column) is under attack by any
 // other queen or not.
-int canPlace(int row, int column){
+int canPlace(int row, int col){
     int i, j;
 
-    // Checking if thre's a queen in row or column
-    for(i = 0; i < boardSize; i++){
-        // borad[row][i] == 1  || board[i][column]
-        if((board[row * boardSize + i] == 1) || (board[i * boardSize + column] == 1))
-            return 0;
+    // Checking the row on left side
+    for(i = 0; i < col; i++){
+        if(board[row * boardSize + i])
+            return false;
     }
 
-    // Checking for diagonals
-    for(i = 0; i < boardSize; i++){
-        for(j = 0; j < boardSize; j++){
-            if(((i+j) == (row+column)) || ((i-j) == (row-column))) {
-                //board[i][j]
-                if(board[i * boardSize + j] == 1)
-                    return 0;
-            }
-        }
+    // Checking the upper diagonal on left side
+    for(i = row, j = col; i >= 0 && j >= 0; i--, j--){
+        if(board[i * boardSize + j])
+            return false;
     }
 
-    // It's all safe here
-    return 1;
+    // CHecking the lower diagonal on left side
+    for(i = row, j = col; j>=0 && i<boardSize ; i++, j--){
+        if(board[i * boardSize + j])
+            return false;
+    }
+
+    return true;
 }
 
 // function where are really implementing 
-// the backtracking algorithm.
-int insertNQueens(int n){
-    int i, j;
+// the backtracking algorithm for put N Queens on board
+int solveNQueens(int col){
 
-    // If n is 0, solution found
-    if(n == 0) return 1;
+    // If all queens are already placed
+    if(col == boardSize){
+        printSolution();
+        return true;
+    }
 
-    for(i = 0; i < boardSize; i++){
-        for(j = 0; j < boardSize; j++){
+    bool res = false;
+    for (int i = 0; i < boardSize; i++){
 
-            // Check if we can place a queen here or not
-            // Queen will not be place if the funcition return false (0)
-            if((canPlace(i, j)) && (board[i * boardSize + j] != 1)) {
+        // Verify if this position isn't under attack
+        if(canPlace(i, col)){
 
-                
-                // Now, this place have a queen
-                board[i * boardSize + j] = 1;
+            // Now, this place have a queen
+            board[i * boardSize + col] = 1;
 
-                // Recursão
-                if(insertNQueens(n - 1) == 1) {
-                    return 1;
-                }
+            // RECURSIVE:
+            // Make result true if any placement is possible
+            res = solveNQueens(col + 1) || res;
 
-                // board[i][j] = 0
-                board[i * boardSize + j] = 0;
-            } 
+            // If this placing queen doesn't lead to a solution
+            // then we remove queen form board[i][col]
+            board[i * boardSize + col] = 0;
             
         }
 
     }
 
-    return 0;
+    return res;
 }
 
-
+// Main function
 int main(int argc, char const *argv[]){
-    
-    // Reading the board size
-    printf("Insert the board size(N): ");
-    scanf("%d", &boardSize);
 
-    // Initializing the board values
+    tempFile = fopen("temp.txt", "w+");
+    if(tempFile == NULL) {
+        printf(">> Erro na criacao de arquivo temporario\n");
+        return 0;
+    }
+
+    // Reading the board size and validating it
+    bool flag = true;
+    while(flag) {
+        printf("Insert the board size(N): ");
+        scanf("%d", &boardSize);
+        if(boardSize > 3 && boardSize < 33) 
+            flag = false;
+        else {
+            printf(">> Entrada deve ser entre 4 e 32\n\n");
+            flag = true;
+        }
+    } 
+
+    // Initializing the board values with 0
     initBoard();
 
-    // Start the algoritgmo
-    insertNQueens(boardSize);
+    // Start the algorithm with 0 queens place
+    if(solveNQueens(0) == false){
+        printf("\n>> Solução não encontrada");
+        fclose(tempFile);
+        return 0;
+    }
 
-    // Print the results
-    printBoard();
-
+    fclose(tempFile);
+    createOutputFile();
+    printf("\n\n>> %d Solutions found!\n", solutionsCount);
+    
     return 0;
 }
